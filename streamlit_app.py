60ecc84d6bddf77a1997c2a46863331577b8e4bd
Veranika <veronica02091990@mail.ru>
2024-04-11 16:02:53 +0200

    Update streamlit_app.py
    Update nutrition dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,9 @@ if ingredients_list:
     ingredients_string = ''
 
     for each_fruit in ingredients_list:
-        ingredients_string += each_fruit + ' '
+        ingredients_string += each_fruit + ' '        
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string +  """', '""" + name_of_smoothier + """')"""
@@ -39,13 +41,9 @@ if ingredients_list:
 
     if time_to_insert:
         if ingredients_string:
-            session.sql(my_insert_stmt).collect()
-        
+            session.sql(my_insert_stmt).collect()        
             st.success('Your Smoothie is ordered, '+ name_of_smoothier + '!', icon="✅")
 
     #st.write(my_insert_stmt)
 
-# New section to display fruityvice nutrition information
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
