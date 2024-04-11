229e28b98a90c5a0414010a35c5212b6e08b3d30
Veranika <veronica02091990@mail.ru>
2024-04-11 15:49:10 +0200

    Update streamlit_app.py
     New section to display fruityvice nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothier :cup_with_straw:")
@@ -42,3 +44,7 @@ if ingredients_list:
             st.success('Your Smoothie is ordered, '+ name_of_smoothier + '!', icon="✅")
 
     #st.write(my_insert_stmt)
+
+# New section to display fruityvice nutrition information
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
